35d11f5c48e68b13fc8250242a70586ac017fc32
Olga Malkovich <olga.malkovich@gmail.com>
2021-03-15 19:30:18 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-module.exports = function toReadable (number) {
+   
+   module.exports = function toReadable (number) {
     let numbers = {
-        0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five', 6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten', 
-        11 : 'elen', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen', 15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen', 19 : 'nineteen', 20 : 'twenty',
-        30 : 'thirty', 40 : 'fourty', 50 : 'fifty', 60 : 'sixty', 70 :        
+        1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine', 10: 'ten',
+        11: 'eleven', 12: 'twelve', 13: 'thirteen', 14: 'fourteen', 15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen', 
+        20: 'twenty', 30: 'thirty', 40: 'forty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',  90: 'ninety',
+       };
+
+    if (number === 0) {
+        return 'zero';
     }
+
+    let readableNumber = '';
+
+    if (number >= 100) {
+        readableNumber += numbers[Math.floor(number / 100)] + ' hundred ';
+    }
+
+    if (number % 100 > 0) {
+        if (number % 100 <= 20){
+            readableNumber += numbers[number % 100];
+        } else {
+            readableNumber += numbers[Math.floor(number % 100 / 10) * 10] + ' ';
+
+            if (number % 10 > 0) {
+                readableNumber += numbers[number % 10];
+            }
+        }
+    }
+
+    return readableNumber.trim();
 }
+
